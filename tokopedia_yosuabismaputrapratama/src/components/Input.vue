f2ed11fba64bc9ad08cbd6d40188ef5f1fdf6f87
yosuabisma <yosuabisma@gmail.com>
2019-07-10 18:25:04 +0700

    function input

--- tokopedia_yosuabismaputrapratama/src/components/Input.vue
+++ tokopedia_yosuabismaputrapratama/src/components/Input.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="input-nominal">
     <input class="input-style" type="text" v-model="message" placeholder="Your Nominal...">
-    <!-- <p>{{message}}</p> -->
     <div class="submit-button" v-on:click="calculateAmount()">Submit</div>
-    <table>
-      <tr>
-        <th class="cell-header">Fraction</th>
-        <th class="cell-header">Amount</th>
-      </tr>
-      <tr v-for="fr in fractions">
-        <td class="cell">Rp {{fr.nominal}}</td>
-        <td class="cell">{{fr.amount}}</td>
-      </tr>
-    </table>
+    <div id="details">
+      <h3>Details</h3>
+      <table>
+        <tr>
+          <th class="cell-header">Fraction</th>
+          <th class="cell-header">Amount</th>
+        </tr>
+        <tr v-for="fr in fractions">
+          <td class="cell">Rp {{fr.nominal}}</td>
+          <td class="cell">{{fr.amount}}</td>
+        </tr>
+      </table>
+      <br/>
+      <font><b>Left: Rp {{left}} (no available fraction)</b></font>
+    </div>
   </div>
 </template>
 <script>
@@ -23,6 +27,7 @@
     data() {
       return {
         message:null,
+        left:0,
         fractions : [
           {nominal:100000, amount:0},
           {nominal:50000, amount:0},
@@ -34,16 +39,26 @@
           {nominal:100, amount:0},
           {nominal:50, amount:0}
         ]
+
       }
     },
     methods:{
       calculateAmount(){
         var nominalInput = this.message;
         var tempFraction = 0;
+        var tempAmount = 0;
         for(var i=0; i<this.fractions.length;i++){
-          var tempAmount = nominalInput/this.fractions[i].nominal;
+          this.fractions[i].amount=0;
+        }
+        for(var i=0; i<this.fractions.length;i++){
+          tempAmount = parseInt(nominalInput/this.fractions[i].nominal);
           if(tempAmount >= 1){
-            this.fractions[i].amount= parseInt(tempAmount);
+            this.fractions[i].amount= tempAmount;
+            tempFraction = this.fractions[i].nominal*tempAmount;
+            nominalInput = nominalInput - tempFraction;
+            if(nominalInput>0){
+              this.left=nominalInput;
+            }
           }else{
             this.fractions[i].amount= 0;
           }
@@ -78,17 +93,34 @@
     color: #e0e0e0;
   }
   .submit-button{
+    margin-top: 8px;
     background-color:#26dc46;
     color: #ffffff;
     cursor: pointer;
+    padding: 10px 10px;
+    width: 400px;
+    border-radius: 8px;
+    font-size: 18px;
   }
-  /* td,th{
+  .submit-button:hover{
+    background-color:#26dc46;
+  }
+  #details{
+    margin-top: 30px;
+  }
+  h3{
+    color:#26dc46;
+    border-bottom: 2px solid #26dc46;
+    width: 100px;
+  }
+  table{
+    width:400px;
+  }
+  td,th{
     padding: 5px 20px;
-    width: 150px;
+    width: 200px;
     text-align: left;
     font-size: 16px;
-    border-radius: 8px;
-
   }
   .cell-header{
     background-color:#26dc46;
@@ -96,5 +128,5 @@
   }
   .cell{
     background-color: #ececec;
-  } */
+  }
 </style>
--- tokopedia_yosuabismaputrapratama/src/components/MainPage.vue
+++ tokopedia_yosuabismaputrapratama/src/components/MainPage.vue
@@ -20,11 +20,6 @@
 </script>
 <style scoped>
   #main-page{
-    /* background-image: url("./../assets/tokopedia-banner-bg-new.png");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100%; */
     margin-top: 40px;
   }
 </style>
