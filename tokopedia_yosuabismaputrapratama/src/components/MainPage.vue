48cc0fa6170669878a2a282a7d9f82cbb66811d2
yosuabisma <yosuabisma@gmail.com>
2019-07-12 03:06:32 +0700

    improve display without footer

--- tokopedia_yosuabismaputrapratama/src/components/MainPage.vue
+++ tokopedia_yosuabismaputrapratama/src/components/MainPage.vue
@@ -2,17 +2,14 @@
   <div id="main-page">
     <center>
       <Input/>
-      <Footer/>
     </center>
   </div>
 </template>
 <script>
   import Input from './Input.vue';
-  import Footer from './Footer.vue';
   export default {
     components: {
-      'Input':Input,
-      'Footer':Footer
+      'Input':Input
     },
     data() {
       return {
